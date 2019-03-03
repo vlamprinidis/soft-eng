cff47025fd54ca5e39910094cbc966ba4dc4a200
vlassis1996 <vlassis.ld@gmail.com>
2019-03-04 01:42:37 +0200

    logout shows user+success message

--- frontend/src/app/logout/logout.component.ts
+++ frontend/src/app/logout/logout.component.ts
@@ -9,16 +9,21 @@ import { Router } from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
   token: string;
+  username: string;
 
   constructor(private jwt: JwtService,  public router: Router) { }
 
   ngOnInit() {
+    if(this.jwt.LoggedIn()){
+      this.username = this.jwt.giveName();
+    }
   }
 
   onClickMe() {
     if (!this.jwt.LoggedIn()) { this.router.navigate(['/login']); return; }
     this.token = this.jwt.giveToken();
     this.jwt.logout(this.token);
+    alert('Αποσυνδέθηκες με επιτυχία!');
     this.router.navigate(['']);
     return;
   }
